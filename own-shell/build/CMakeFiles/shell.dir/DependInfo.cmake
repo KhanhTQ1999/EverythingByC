420e54cf2ed4c1341b19fa910ffb54ba72fb0848
Khanh <trinhquockhanh2012199@gmail.com>
2026-04-20 21:20:37 +0700

    feat: update new own-shell

---
+++ own-shell/build/CMakeFiles/shell.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/khanh/Work/EverythingByC/own-shell/main.c" "/home/khanh/Work/EverythingByC/own-shell/build/CMakeFiles/shell.dir/main.c.o"
+  "/home/khanh/Work/EverythingByC/own-shell/src/app_state.c" "/home/khanh/Work/EverythingByC/own-shell/build/CMakeFiles/shell.dir/src/app_state.c.o"
+  "/home/khanh/Work/EverythingByC/own-shell/src/executor.c" "/home/khanh/Work/EverythingByC/own-shell/build/CMakeFiles/shell.dir/src/executor.c.o"
+  "/home/khanh/Work/EverythingByC/own-shell/src/parser.c" "/home/khanh/Work/EverythingByC/own-shell/build/CMakeFiles/shell.dir/src/parser.c.o"
+  "/home/khanh/Work/EverythingByC/own-shell/src/utils.c" "/home/khanh/Work/EverythingByC/own-shell/build/CMakeFiles/shell.dir/src/utils.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- typedef.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-typedef uint64_t    u64;
-typedef uint8_t     u8;
-
